401cd880c1ecf628df498bbfe1f4c00504427258
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2026-02-25 15:08:45 +0530

    Client controller established

---
+++ myApplication/src/main/java/com/project/myApplication/Controller/ClientController.java
@@ -0,0 +1,24 @@
+package com.project.myApplication.Controller;
+
+import com.project.myApplication.DTO.Client;
+import com.project.myApplication.Response.ResponseStructure;
+import com.project.myApplication.Service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("client")
+public class ClientController {
+
+    @Autowired
+    private ClientService clser;
+
+    @PostMapping
+    public ResponseEntity<ResponseStructure<Client>> saveClient(@RequestBody Client c) {
+        return clser.saveCl(c);
+    }
+}
