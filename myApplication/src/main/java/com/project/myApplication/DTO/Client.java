32616694865c4e6dc91d6fbc67ef151224abb4e4
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2026-02-25 14:30:58 +0530

    Client Entity creation

---
+++ myApplication/src/main/java/com/project/myApplication/DTO/Client.java
@@ -0,0 +1,49 @@
+package com.project.myApplication.DTO;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto-generate id
+    private int id;
+    private String name;
+    private String email;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+}
