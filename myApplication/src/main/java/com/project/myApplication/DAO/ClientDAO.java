ee2c41fe9c2569efa798bdf02c3706d10f9235ea
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2026-02-25 15:23:17 +0530

    Fetch using name

--- myApplication/src/main/java/com/project/myApplication/DAO/ClientDAO.java
+++ myApplication/src/main/java/com/project/myApplication/DAO/ClientDAO.java
@@ -4,6 +4,7 @@ import com.project.myApplication.DTO.Client;
 import com.project.myApplication.Repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public class ClientDAO {
@@ -13,5 +14,10 @@ public class ClientDAO {
 
     public Client saveClient(Client c) {
         return clres.save(c);
+    }
+
+    public String getByName(String name) {
+        List<Client> list = clres.findByName(name);
+        return "Records are: " + list;
     }
 }
