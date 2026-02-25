8502700089cfb75b4c2690b97002eb59c35deb89
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2026-02-25 14:47:05 +0530

    Created Exception Handler

---
+++ myApplication/src/main/java/com/project/myApplication/Exception/HandleException.java
@@ -0,0 +1,24 @@
+package com.project.myApplication.Exception;
+
+import com.project.myApplication.DTO.Client;
+import com.project.myApplication.Response.ResponseStructure;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class HandleException {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseStructure<String>> handle(IDNotFoundException e) {
+        ResponseStructure<String> res = new ResponseStructure<>();
+
+        res.setStatusCode(HttpStatus.NOT_FOUND.value());
+        res.setMessage("Give existing ID");
+        res.setData(e.getMessage());
+
+        return new ResponseEntity<>(res, HttpStatus.NOT_FOUND);
+    }
+}
